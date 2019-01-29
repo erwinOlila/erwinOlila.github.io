b191f19b2253e5f146093c724493a551941ce295
erwinOlila <ce.olila@gmail.com>
2019-01-29 23:18:33 +0800

    added responsiveness from thingspeak

---
+++ assets/thingspeak/thingspeak.ts
@@ -0,0 +1,8 @@
+export const Thing = {
+    channel: 488600,
+    host: 'ws://mqtt.thingspeak.com/mqtt',
+    username: 'user',
+    mqtt_key: 'HC57VHW8JOU8OIJ4',
+    read_key: 'P6HKGL4J2BOR2JS9',
+    write_key: 'BWOUM1ZIP9D3IIKS'
+};
