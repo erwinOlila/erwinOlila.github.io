8dd517024681cc15acf8cb63d108cdc8b8dc5a99
erwinOlila <ce.olila@gmail.com>
2020-09-10 11:54:46 +0800

    Updates

---
+++ precache-manifest.dee7bf30c6f2c7526b09c0d511530ab1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0297cbe8502cea933a826d161a11dd9",
+    "url": "/airwayze/index.html"
+  },
+  {
+    "revision": "52316529e6d195c30720",
+    "url": "/airwayze/static/css/main.76f4f183.chunk.css"
+  },
+  {
+    "revision": "c8844e1035ed013b5761",
+    "url": "/airwayze/static/js/2.3ceb0add.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/airwayze/static/js/2.3ceb0add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52316529e6d195c30720",
+    "url": "/airwayze/static/js/main.6bde7f3a.chunk.js"
+  },
+  {
+    "revision": "bc06f904f2926b9a199d",
+    "url": "/airwayze/static/js/runtime-main.576ae419.js"
+  },
+  {
+    "revision": "07505f3f1fa9f1850c68c26f3395d43b",
+    "url": "/airwayze/static/media/breezometer.07505f3f.png"
+  },
+  {
+    "revision": "74c9657e8f0e5f1ceeeb0f2af4857989",
+    "url": "/airwayze/static/media/city2.74c9657e.jpg"
+  },
+  {
+    "revision": "84c3c0b85581b5c6212268a1a1811db9",
+    "url": "/airwayze/static/media/problem3.84c3c0b8.jpg"
+  }
+]);
