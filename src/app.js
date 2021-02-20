b4ddc5acc32cf819da379f93a01565a14814a0d5
erwinOlila <ce.olila@gmail.com>
2021-02-20 20:25:02 +0800

    formatted labels to datetime

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-        apiKey: "AIzaSyA9Qtxo66LWPtAAvQFJ_EAjB2NTwKn2lDA",
-        authDomain: "home-temperature-data-1ef85.firebaseapp.com",
-        databaseURL: "https://home-temperature-data-1ef85-default-rtdb.firebaseio.com",
-        projectId: "home-temperature-data-1ef85",
-        storageBucket: "home-temperature-data-1ef85.appspot.com",
-        messagingSenderId: "527187977802",
-        appId: "1:527187977802:web:e9ff71fe661976cbb7a8b3",
-        measurementId: "G-WDJD6JLPQX"
+    apiKey: "AIzaSyA9Qtxo66LWPtAAvQFJ_EAjB2NTwKn2lDA",
+    authDomain: "home-temperature-data-1ef85.firebaseapp.com",
+    databaseURL: "https://home-temperature-data-1ef85-default-rtdb.firebaseio.com",
+    projectId: "home-temperature-data-1ef85",
+    storageBucket: "home-temperature-data-1ef85.appspot.com",
+    messagingSenderId: "527187977802",
+    appId: "1:527187977802:web:e9ff71fe661976cbb7a8b3",
+    measurementId: "G-WDJD6JLPQX"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -15,17 +15,20 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 var dataRef  = database.ref('/');
 dataRef.limitToLast(20).get().then(function(snapshot){
-    if (snapshot.exists()) {
-        const data = snapshot.val();
-        const keys = Object.keys(data);
-        const vals = Object.values(data);
-        plottData(keys, vals);
-    }
-    else {
-        console.log("No data available");
-    }
+if (snapshot.exists()) {
+    const data = snapshot.val();
+    const keys = Object.keys(data);
+    const vals = Object.values(data);
+    keys.forEach(function(key, i) {
+        this[i] = convertTime(key);
+    }, keys);
+    plottData(keys, vals);
+}
+else {
+    console.log("No data available");
+}
 }).catch(function(error){
-    console.error(error);
+console.error(error);
 })
 
 // dataRef.limitToLast(10).on('child_added', function(data) {
@@ -35,90 +38,64 @@ dataRef.limitToLast(20).get().then(function(snapshot){
 //     console.log(sensor_data.date + ': ' + sensor_data.value)
 // })
 
-function plottData(k, v) {
-    var ctx = document.getElementById('myChart');
-    const newLocal = 'cyan';
-    var config = {
-        type: 'line',
-        data: {
-            labels: k,
-            datasets: [{
-                label: 'Temperature',
-                backgroundColor: 'red',
-                borderColor: 'red',
-                data: v,
-                fill:false
-            }]
-        },
-        options: {
-            responsive: true,
-            title: {
-                display: true,
-                text: 'Sensor Data'
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            }
-        }
-    }
-    var myChart = new Chart(ctx, config);
+function convertTime(unix) {
+unix = unix - 28800;
+var time = new Date(unix * 1000);
+var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
-    dataRef.limitToLast(1).on('child_added', (snapshot) => {
-        console.log(snapshot.key);
-        const key = snapshot.key;
-        const val = snapshot.val();
+var year = time.getFullYear();
+var month = months[time.getMonth()]
+var date = time.getDate();
+var hour = time.getHours();
+var min = time.getMinutes();
+var sec = time.getSeconds();
 
-        if (config.data.labels.includes(key)) {
-            console.log("already exists");
-        } else {
-            config.data.labels.push(key);
-            config.data.datasets.forEach(function(dataset) {
-                dataset.data.push(val);
-            });
-            myChart.update();
-        }
-    })
+var str_time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+
+return str_time;
 }
 
+function plottData(k, v) {
+var ctx = document.getElementById('myChart');
+const newLocal = 'cyan';
+var config = {
+    type: 'line',
+    data: {
+        labels: k,
+        datasets: [{
+            label: 'Temperature',
+            backgroundColor: 'red',
+            borderColor: 'red',
+            data: v,
+            fill:false
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Sensor Data'
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        }
+    }
+}
+var myChart = new Chart(ctx, config);
 
-
-
-
-// var ctx = document.getElementById('myChart');
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
+dataRef.limitToLast(1).on('child_added', (snapshot) => {
+    console.log(snapshot.key);
+    const key = convertTime(snapshot.key);
+    const val = snapshot.val();
+    if (config.data.labels.includes(key)) {
+        console.log("already exists");
+    } else {
+        config.data.labels.push(key);
+        config.data.datasets.forEach(function(dataset) {
+            dataset.data.push(val);
+        });
+        myChart.update();
+    }
+})
+}
